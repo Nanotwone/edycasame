e50ce6fec5840612319a51e55e70cb737261cc38
Nanotwone <nanotwone@gmail.com>
2026-02-17 14:18:48 +0700

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,1 +1,16 @@
+import json
+
+def handler(request):
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"status": "ok"})
+        }
+
+    return {
+        "statusCode": 200,
+        "body": "Bot is running"
+    }
 
